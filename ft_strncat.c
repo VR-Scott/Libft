7eb094a27ae1d390ddd2b58c6c58774611fa7174
Vaughan SCOTT <vscott@c4r8s10.wethinkcode.co.za>
2019-05-21 15:51:57 +0200

    commiting strncat

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,29 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 13:08:24 by vscott            #+#    #+#             */
-/*   Updated: 2019/05/21 14:11:57 by vscott           ###   ########.fr       */
+/*   Created: 2019/05/21 14:18:28 by vscott            #+#    #+#             */
+/*   Updated: 2019/05/21 15:47:32 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcat(char *s1, const char *s2)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	char *src;
-	char *dst;
+	char	*src;
+	char	*dst;
 
 	dst = s1;
 	src = (char *)s2;
 	while (*dst)
 		dst++;
-	while (*src)
+	while (*src && n > 0)
 	{
 		*dst = *src;
 		dst++;
 		src++;
+		n--;
 	}
 	*dst = '\0';
 	return (s1);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 08:38:25 by vscott            #+#    #+#             */
-/*   Updated: 2019/05/21 14:18:42 by vscott           ###   ########.fr       */
+/*   Updated: 2019/05/21 15:51:21 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,5 @@ void	ft_strclr(char *s);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst,const char *src, size_t len);
 char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
 #endif
