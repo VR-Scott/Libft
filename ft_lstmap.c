26ef36d649cf9041a1a90ac540853fe9ba4f6697
Vaughan SCOTT <vscott@c4r8s10.wethinkcode.co.za>
2019-06-02 12:19:01 +0200

    norm of lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,23 +6,23 @@
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:46:17 by vscott            #+#    #+#             */
-/*   Updated: 2019/05/31 12:02:55 by vscott           ###   ########.fr       */
+/*   Updated: 2019/06/02 12:17:25 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-		if (lst && f)
-		{
-			t_list *fresh;
+	t_list *fresh;
 
-			if (!(fresh = (t_list*)malloc(sizeof(t_list))))
-				return (NULL);
-			fresh = f(lst);
-			fresh->next = ft_lstmap(lst->next, f);
-			return (fresh);
-		}
-		return (NULL);
+	if (lst && f)
+	{
+		if (!(fresh = (t_list*)malloc(sizeof(t_list))))
+			return (NULL);
+		fresh = f(lst);
+		fresh->next = ft_lstmap(lst->next, f);
+		return (fresh);
+	}
+	return (NULL);
 }
